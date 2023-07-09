02c853cd7df4266efefe1558d1fdfb46fb3f743a
keterum <keterum@gmail.com>
2023-07-09 12:42:59 +0300

    09.07.2023

---
+++ utils/assertions.py
@@ -0,0 +1,13 @@
+from jsonschema import validate
+from pydantic import BaseModel
+
+class Assert:
+    @staticmethod
+    def validate_schema(instance: dict) -> None:
+        validate(instance=instance, schema=BaseModel.schema())
+        # validate(instance=instance, schema=BaseModel.model_json_schema())
+        # validate(instance=instance, schema=BaseModel.schema())
+        # model_json_schema
+        # cls.check_schema(schema)
+
+
